2c344cb596fd50191f1adc8692478a07078826af
anhtuan <anhltse.fpt@gmail.com>
2020-08-09 12:10:18 -0600

    [f]Add Git ignore

---
+++ chapter_1/arrow_function.js
@@ -0,0 +1,24 @@
+'use strict'
+
+function DelayedGreeter (name) {
+    this.name = name
+}
+
+DelayedGreeter.prototype.greet = function () {
+    setTimeout(() => console.log('Hello ' + this.name), 500)
+}
+
+DelayedGreeter.prototype.greet1 = function () {
+    setTimeout(function cb() {
+        console.log('Hello ' + this.name)
+    }, 500)
+}
+
+DelayedGreeter.prototype.greet2= function() {
+    setTimeout((function cb() {
+        console.log('Hello ' + this.name)
+    }).bind(this), 500)
+}
+
+const greeter = new DelayedGreeter('World')
+greeter.greet2()
